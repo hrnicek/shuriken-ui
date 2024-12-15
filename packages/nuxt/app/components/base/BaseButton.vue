9693b695a159a10036c7b4a8b2258703dee8a01b
Sacha STAFYNIAK <sacha@digisquad.io>
2024-12-15 14:47:13 +0100

    chore: update button interface

--- packages/nuxt/app/components/base/BaseButton.vue
+++ packages/nuxt/app/components/base/BaseButton.vue
@@ -1,60 +1,59 @@
-<script setup lang="ts">
+<script lang="ts">
 import type { RouteLocationRaw } from 'vue-router'
 
-const props = withDefaults(
-  defineProps<{
-    /** The location to which the button should navigate when clicked. This is only applicable if the button is a link. */
-    to?: RouteLocationRaw
+interface BaseButtonProps {
+  /** The location to which the button should navigate when clicked. This is only applicable if the button is a link. */
+  to?: RouteLocationRaw
 
-    /** Using href instead of to result in a native anchor with no router functionality. */
-    href?: string
+  /** Using href instead of to result in a native anchor with no router functionality. */
+  href?: string
 
-    /** The value of the 'rel' attribute of the button. This attribute is used to specify the relationship of the linked document with the current document. */
-    rel?: string
+  /** The value of the 'rel' attribute of the button. This attribute is used to specify the relationship of the linked document with the current document. */
+  rel?: string
 
-    /** The value of the 'target' attribute of the button. This attribute is used to specify where to open the linked document. */
-    target?: string
+  /** The value of the 'target' attribute of the button. This attribute is used to specify where to open the linked document. */
+  target?: string
 
-    /** The type of the button. */
-    type?: 'button' | 'submit' | 'reset'
+  /** The type of the button. */
+  type?: 'button' | 'submit' | 'reset'
 
-    /** Whether the button is in a loading state. */
-    loading?: boolean
+  /** Whether the button is in a loading state. */
+  loading?: boolean
 
-    /**
-     * The variant of the button..
-     *
-     * @since 2.0.0
-     * @default 'default'
-     */
-    variant?: 'default-low' | 'default-high' |'muted-low' | 'muted-high' | 'ghost-low' | 'ghost-high' | 'primary' | 'destructive' | 'dark' | 'link' | 'custom'
+  /**
+   * The variant of the button..
+   *
+   * @default 'default'
+   */
+  variant?: 'default-low' | 'default-high' |'muted-low' | 'muted-high' | 'ghost-low' | 'ghost-high' | 'primary' | 'destructive' | 'dark' | 'link' | 'custom'
 
-    /**
-     * The size of the button
-     *
-     * @default 'md'
-     */
-    size?: 'sm' | 'icon-sm' | 'md' | 'icon-md' | 'lg' | 'icon-lg' | 'xl' | 'icon-xl'
+  /**
+   * The size of the button
+   *
+   * @default 'md'
+   */
+  size?: 'sm' | 'icon-sm' | 'md' | 'icon-md' | 'lg' | 'icon-lg' | 'xl' | 'icon-xl'
 
-    /**
-     * The radius of the button.
-     *
-     * @since 2.0.0
-     * @default 'sm'
-     */
-    rounded?: 'none' | 'sm' | 'md' | 'lg' | 'full'
-  }>(),
-  {
-    to: undefined,
-    href: undefined,
-    rel: '',
-    target: '',
-    type: undefined,
-    variant: undefined,
-    size: undefined,
-    rounded: undefined,
-  },
-)
+  /**
+   * The radius of the button.
+   *
+   * @default 'sm'
+   */
+  rounded?: 'none' | 'sm' | 'md' | 'lg' | 'full'
+}
+</script>
+
+<script setup lang="ts">
+const props = withDefaults(defineProps<BaseButtonProps>(), {
+  to: undefined,
+  href: undefined,
+  rel: '',
+  target: '',
+  type: undefined,
+  variant: undefined,
+  size: undefined,
+  rounded: undefined,
+})
 
 const variant = useNuiDefaultProperty(props, 'BaseButton', 'variant')
 const size = useNuiDefaultProperty(props, 'BaseButton', 'size')
@@ -69,7 +68,7 @@ const sizes = {
   'icon-md': 'size-10 text-sm',
   'icon-lg': 'size-12 text-base',
   'icon-xl': 'size-14 text-base',
-}
+} as const
 
 const radiuses = {
   none: '',
@@ -77,7 +76,7 @@ const radiuses = {
   md: 'rounded-lg',
   lg: 'rounded-xl',
   full: 'rounded-full',
-}
+} as const
 
 const variants = {
   'default-low': 'border border-muted-300 dark:border-muted-600 hover:enabled:border-muted-200 dark:hover:enabled:border-muted-500 text-muted-700 dark:text-muted-100 bg-white dark:bg-muted-700 hover:enabled:bg-muted-50 dark:hover:enabled:bg-muted-600 active:enabled:bg-white dark:active:enabled:bg-muted-700 focus-visible:bg-muted-50 dark:focus-visible:bg-muted-600 ',
@@ -91,7 +90,7 @@ const variants = {
   'dark': 'text-muted-100 dark:text-muted-900 bg-muted-900 dark:bg-muted-100 hover:enabled:bg-muted-700 dark:hover:enabled:bg-muted-100 active:enabled:bg-muted-900 dark:active:enabled:bg-muted-50 focus-visible:bg-muted-800 dark:focus-visible:bg-muted-50 border border-muted-900 dark:border-muted-100 border-muted-800 dark:border-muted-50',
   'link': 'text-muted-600 dark:text-muted-100 hover:text-primary-500 dark:hover:text-primary-500 focus-visible:text-primary-500 dark:focus-visible:text-primary-500 hover:underline underline-offset-4',
   'custom': '',
-}
+} as const
 
 const classes = computed(() => [
   'relative inline-flex justify-center items-center space-x-1 font-sans font-normal leading-5 no-underline transition-all duration-300 disabled:opacity-60 disabled:cursor-not-allowed hover:shadow-none',
