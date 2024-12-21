6e0a8eef9a574dc4015c14eaff9a43eefda5c57a
Sacha STAFYNIAK <sacha@digisquad.io>
2024-12-21 11:04:07 +0100

    feat: update theme system to reka ui

--- packages/nuxt/app/components/base/BaseThemeSystem.vue
+++ packages/nuxt/app/components/base/BaseThemeSystem.vue
@@ -78,23 +78,23 @@ const preference = computed({
 </script>
 
 <template>
-  <div class="relative p-1 rounded-full max-w-[104px]" :class="trackVariants[variant]">
+  <RadioGroupRoot v-model="preference" class="relative p-1 rounded-full max-w-[104px]" :class="trackVariants[variant]">
     <div class="relative flex">
-      <button class="relative z-10 size-8 flex items-center justify-center" role="option" @click="preference = 'system'">
+      <RadioGroupItem class="relative z-10 size-8 flex items-center justify-center nui-focus rounded-full" value="system" @click="preference = 'system'">
         <span class="pointer-events-none">
           <Icon class="text-lg" :name="iconScreen" :class="preference === 'system' ? 'text-primary-500' : 'text-muted-400 dark:text-muted-500'" />
         </span>
-      </button>
-      <button class="relative z-10 size-8 flex items-center justify-center" role="option" @click="preference = 'light'">
+      </RadioGroupItem>
+      <RadioGroupItem class="relative z-10 size-8 flex items-center justify-center nui-focus rounded-full" value="light" @click="preference = 'light'">
         <span class="pointer-events-none">
           <Icon class="text-lg" :name="iconSun" :class="preference === 'light' ? 'text-yellow-400' : 'text-muted-400 dark:text-muted-500'" />
         </span>
-      </button>
-      <button class="relative z-10 size-8 flex items-center justify-center" role="option" @click="preference = 'dark'">
+      </RadioGroupItem>
+      <RadioGroupItem class="relative z-10 size-8 flex items-center justify-center nui-focus rounded-full" value="dark" @click="preference = 'dark'">
         <span class="pointer-events-none">
           <Icon class="text-lg" :name="iconMoon" :class="preference === 'dark' ? 'text-yellow-400' : 'text-muted-400 dark:text-muted-500'" />
         </span>
-      </button>
+      </RadioGroupItem>
       <div
         class="absolute top-0 left-0 w-8 h-8 rounded-full z-0 transition-all duration-300"
         :class="[
@@ -106,5 +106,5 @@ const preference = computed({
       >
       </div>
     </div>
-  </div>
+  </RadioGroupRoot>
 </template>
