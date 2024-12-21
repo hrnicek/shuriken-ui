0d1f46a14639a143be1cc93b74412225eee7641c
Sacha STAFYNIAK <sacha@digisquad.io>
2024-12-21 11:45:05 +0100

    chore: add fieldset disabled test

--- playgrounds/nuxt-4/app/pages/tests/tests/form-fields.vue
+++ playgrounds/nuxt-4/app/pages/tests/tests/form-fields.vue
@@ -10,101 +10,103 @@ definePageMeta({
   <div>
     <NuiPreviewContainer title="Forms">
       <NuiPreview title="Form" description="Form sumbission">
-        <form class="grid grid-cols-2 gap-8 max-w-2xl">
-          <BaseField name="first-name" required>
-            <BaseFieldLabel>
-              <span>First name</span> 
-              <BaseFieldRequiredIndicator  />
-            </BaseFieldLabel>
+        <form class=" max-w-2xl">
+          <fieldset class="grid grid-cols-2 gap-8" disabled>
+            <BaseField name="first-name" required>
+              <BaseFieldLabel>
+                <span>First name</span> 
+                <BaseFieldRequiredIndicator  />
+              </BaseFieldLabel>
 
-            <div class="relative w-full">
-              <BaseFieldController>
-                <BaseInput class="w-full" autocomplete="given-name" />
-              </BaseFieldController>
-              <div class="absolute z-10 end-4 top-3" >
-                <BaseFieldLoadingIndicator/>
-                <BaseFieldSuccessIndicator />
-                <BaseFieldErrorIndicator />
+              <div class="relative w-full">
+                <BaseFieldController>
+                  <BaseInput class="w-full" autocomplete="given-name" />
+                </BaseFieldController>
+                <div class="absolute z-10 end-4 top-3" >
+                  <BaseFieldLoadingIndicator/>
+                  <BaseFieldSuccessIndicator />
+                  <BaseFieldErrorIndicator />
+                </div>
               </div>
-            </div>
-          </BaseField>
-          
-          <BaseField name="last-name" required>
-            <BaseFieldLabel>
-              <span>Last name</span> 
-              <BaseFieldRequiredIndicator  />
-            </BaseFieldLabel>
+            </BaseField>
+            
+            <BaseField name="last-name" required>
+              <BaseFieldLabel>
+                <span>Last name</span> 
+                <BaseFieldRequiredIndicator  />
+              </BaseFieldLabel>
 
-            <div class="relative w-full">
-              <BaseFieldController>
-                <BaseInput class="w-full" autocomplete="family-name" />
-              </BaseFieldController>
-              <div class="absolute z-10 end-4 top-3" >
-                <BaseFieldLoadingIndicator/>
-                <BaseFieldSuccessIndicator />
-                <BaseFieldErrorIndicator />
+              <div class="relative w-full">
+                <BaseFieldController>
+                  <BaseInput class="w-full" autocomplete="family-name" />
+                </BaseFieldController>
+                <div class="absolute z-10 end-4 top-3" >
+                  <BaseFieldLoadingIndicator/>
+                  <BaseFieldSuccessIndicator />
+                  <BaseFieldErrorIndicator />
+                </div>
               </div>
-            </div>
-          </BaseField>
+            </BaseField>
 
-          
-          <BaseField name="email" required class="col-span-2">
-            <BaseFieldLabel>
-              <span>Email</span> 
-              <BaseFieldRequiredIndicator  />
-            </BaseFieldLabel>
+            
+            <BaseField name="email" required class="col-span-2">
+              <BaseFieldLabel>
+                <span>Email</span> 
+                <BaseFieldRequiredIndicator  />
+              </BaseFieldLabel>
 
-            <div class="relative w-full">
-              <BaseFieldController>
-                <BaseInput type="email" class="w-full" autocomplete="email" />
-              </BaseFieldController>
-              <div class="absolute z-10 end-4 top-3" >
-                <BaseFieldLoadingIndicator/>
-                <BaseFieldSuccessIndicator />
-                <BaseFieldErrorIndicator />
+              <div class="relative w-full">
+                <BaseFieldController>
+                  <BaseInput type="email" class="w-full" autocomplete="email" />
+                </BaseFieldController>
+                <div class="absolute z-10 end-4 top-3" >
+                  <BaseFieldLoadingIndicator/>
+                  <BaseFieldSuccessIndicator />
+                  <BaseFieldErrorIndicator />
+                </div>
               </div>
-            </div>
-          </BaseField>
+            </BaseField>
 
-          <BaseField name="tier" required>
-            <BaseFieldLabel>
-              <span>Select tier</span> 
-              <BaseFieldRequiredIndicator  />
-            </BaseFieldLabel>
+            <BaseField name="tier" required>
+              <BaseFieldLabel>
+                <span>Select tier</span> 
+                <BaseFieldRequiredIndicator  />
+              </BaseFieldLabel>
 
-            <div class="relative w-full">
-              <BaseFieldController>
-                <BaseSelect>
-                  <BaseSelectItem value="1">Option 1</BaseSelectItem>
-                  <BaseSelectItem value="2">Option 2</BaseSelectItem>
-                  <BaseSelectItem value="3">Option 3</BaseSelectItem>
-                </BaseSelect>
-              </BaseFieldController>
-              <div class="absolute z-10 end-4 top-3" >
-                <BaseFieldLoadingIndicator/>
-                <BaseFieldSuccessIndicator />
-                <BaseFieldErrorIndicator />
+              <div class="relative w-full">
+                <BaseFieldController>
+                  <BaseSelect>
+                    <BaseSelectItem value="1">Option 1</BaseSelectItem>
+                    <BaseSelectItem value="2">Option 2</BaseSelectItem>
+                    <BaseSelectItem value="3">Option 3</BaseSelectItem>
+                  </BaseSelect>
+                </BaseFieldController>
+                <div class="absolute z-10 end-4 top-3" >
+                  <BaseFieldLoadingIndicator/>
+                  <BaseFieldSuccessIndicator />
+                  <BaseFieldErrorIndicator />
+                </div>
               </div>
-            </div>
-          </BaseField>
+            </BaseField>
 
-          <BaseField name="agree-terms" class="col-span-2">
-            <BaseFieldLabel>
-              <span>Aggree ?</span> 
-              <BaseFieldRequiredIndicator  />
-            </BaseFieldLabel>
+            <BaseField name="agree-terms" class="col-span-2">
+              <BaseFieldLabel>
+                <span>Aggree ?</span> 
+                <BaseFieldRequiredIndicator  />
+              </BaseFieldLabel>
 
-            <div class="relative w-full">
-              <BaseFieldController>
-                <BaseSwitchBall />
-              </BaseFieldController>
-              <div class="absolute z-10 end-4 top-3" >
-                <BaseFieldLoadingIndicator/>
-                <BaseFieldSuccessIndicator />
-                <BaseFieldErrorIndicator />
+              <div class="relative w-full">
+                <BaseFieldController>
+                  <BaseSwitchBall />
+                </BaseFieldController>
+                <div class="absolute z-10 end-4 top-3" >
+                  <BaseFieldLoadingIndicator/>
+                  <BaseFieldSuccessIndicator />
+                  <BaseFieldErrorIndicator />
+                </div>
               </div>
-            </div>
-          </BaseField>
+            </BaseField>
+          </fieldset>
 
           <div class="col-span-2">
             <BaseButton type="submit" class="w-full">Submit</BaseButton>
