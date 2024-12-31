33a330252420ecd6d796e3274ff807d05d049e18
Sacha STAFYNIAK <sacha@digisquad.io>
2024-12-31 17:56:59 +0100

    feat: improve breadcrumb a11y

--- packages/nuxt/app/components/base/BaseBreadcrumb.vue
+++ packages/nuxt/app/components/base/BaseBreadcrumb.vue
@@ -104,63 +104,60 @@ const items = computed(() => {
 </script>
 
 <template>
-  <nav class="block">
-    <ul class="mb-6 flex items-center font-sans text-[0.85rem]">
-      <li class="me-3 sm:hidden">
-        <BaseDropdown
-          size="md"
+  <nav aria-label="Breadcrumb">
+    <BaseDropdown
+      class="me-3 sm:hidden"
+      size="md"
+    >
+      <template #button>
+        <BaseButton
+          size="icon-sm"
+          rounded="full"
+          class="group"
         >
-          <template #button>
-            <BaseButton
-              size="icon-sm"
-              rounded="full"
-              class="group"
-            >
-              <Icon name="lucide:ellipsis" class="block text-base transition-transform group-data-[state=open]:rotate-90" />
-            </BaseButton>
-          </template>
-          <BaseDropdownItem
-            v-for="(item, index) in items.slice(0, items.length - 1)"
-            :key="index"
-            :to="item.to"
-            class="flex items-center gap-x-1"
-          >
-            {{ item.label }}
-          </BaseDropdownItem>
-        </BaseDropdown>
-      </li>
-      <template v-for="(item, index) in items" :key="index">
-        <li
-          class="not-last:hidden not-last:sm:flex last:flex last:text-muted-500 dark:last:text-muted-400"
-          :class="[
-            index !== items.length - 1 ? 'hidden sm:flex' : 'flex',
-          ]"
-        >
-          <slot name="link" v-bind="{ item, index }">
-            <NuxtLink
-              :to="item.to"
-              class="hover:underline underline-offset-4 text-[0.85rem] flex items-center gap-x-1 text-muted-500 dark:text-muted-400 transition-colors duration-300"
-              :class="[
-                item.to && variant === 'primary' && 'hover:text-primary-heavy focus:text-primary-heavy dark:hover:text-primary-light dark:focus:text-primary-light',
-                item.to && variant === 'dark' && 'hover:text-muted-900 focus:text-muted-900 dark:hover:text-muted-100 dark:focus:text-muted-100',
-              ]"
-            >
-              <slot name="label" v-bind="{ item, index }">
-                <span :class="[item.hideLabel && 'sr-only']">
-                  {{ item.label }}
-                </span>
-              </slot>
-            </NuxtLink>
-          </slot>
-        </li>
-        <li class="not-last:hidden not-last:sm:flex last:flex last:text-muted-500 dark:last:text-muted-400">
-          <div class="text-[0.85rem] flex items-center gap-x-1 text-muted-500 dark:text-muted-400 transition-colors duration-300">
-            <span v-if="index < items.length - 1" class="text-muted-500 dark:text-muted-400 px-2">
-              <slot>·</slot>
-            </span>
-          </div>
-        </li>
+          <Icon name="lucide:ellipsis" class="block text-base transition-transform group-data-[state=open]:rotate-90" />
+        </BaseButton>
       </template>
-    </ul>
+      <BaseDropdownItem
+        v-for="(item, index) in items.slice(0, items.length - 1)"
+        :key="index"
+        :to="item.to"
+        class="flex items-center gap-x-1"
+      >
+        {{ item.label }}
+      </BaseDropdownItem>
+    </BaseDropdown>
+    <ol class="mb-6 flex items-center font-sans text-[0.85rem]">
+      <li
+        v-for="(item, index) in items"
+        :key="index"
+        class="not-last:hidden not-last:sm:flex last:flex last:text-muted-500 dark:last:text-muted-400"
+        :class="[
+          index !== items.length - 1 ? 'hidden sm:flex' : 'flex',
+        ]"
+      >
+        <slot name="link" v-bind="{ item, index }">
+          <NuxtLink
+            :to="item.to"
+            class="hover:underline underline-offset-4 text-[0.85rem] flex items-center gap-x-1 text-muted-500 dark:text-muted-400 transition-colors duration-300"
+            :class="[
+              item.to && variant === 'primary' && 'hover:text-primary-heavy focus:text-primary-heavy dark:hover:text-primary-light dark:focus:text-primary-light',
+              item.to && variant === 'dark' && 'hover:text-muted-900 focus:text-muted-900 dark:hover:text-muted-100 dark:focus:text-muted-100',
+            ]"
+          >
+            <slot name="label" v-bind="{ item, index }">
+              <span :class="[item.hideLabel && 'sr-only']">
+                {{ item.label }}
+              </span>
+            </slot>
+          </NuxtLink>
+        </slot>
+        <div v-if="index < items.length - 1" class="text-[0.85rem] flex items-center gap-x-1 text-muted-500 dark:text-muted-400 transition-colors duration-300">
+          <span class="text-muted-500 dark:text-muted-400 px-2">
+            <slot>·</slot>
+          </span>
+        </div>
+      </li>
+    </ol>
   </nav>
 </template>
