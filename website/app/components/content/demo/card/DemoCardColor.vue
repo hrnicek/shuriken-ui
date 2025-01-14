42bf543bd80fb35cf9bf0754ec96d3af5021c06d
Driss Chelouati <driss.chelouati@gmail.com>
2025-01-14 20:25:46 +0100

    feat: remove deprecated button demo components

--- website/app/components/content/demo/card/DemoCardColor.vue
+++ website/app/components/content/demo/card/DemoCardColor.vue
@@ -2,194 +2,66 @@
   <div class="flex items-center justify-center px-4 pb-0 pt-4">
     <div class="w-full rounded-xl bg-muted-50 p-4 dark:bg-muted-900 md:p-8">
       <div class="grid max-w-3xl gap-4 md:grid-cols-2">
-        <BaseCard
-          rounded="md"
-          color="default"
-          class="p-6"
-        >
+        <BaseCard  variant="default" rounded="md" class="p-6">
           <BaseHeading
             as="h4"
             size="sm"
             weight="semibold"
             lead="tight"
-            class="mb-2 text-muted-800 dark:text-white"
+            class="text-muted-800 mb-2 dark:text-white"
           >
-            Iam a card
+            Default card
           </BaseHeading>
 
-          <BaseParagraph
-            size="sm"
-            lead="tight"
-            class="text-muted-400"
-          >
+          <BaseParagraph size="sm" lead="tight" class="text-muted-400">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </BaseParagraph>
         </BaseCard>
 
-        <BaseCard
-          rounded="md"
-          color="primary"
-          class="p-6"
-        >
+        <BaseCard variant="muted" rounded="md" class="p-6">
           <BaseHeading
             as="h4"
             size="sm"
             weight="semibold"
             lead="tight"
-            class="mb-2 text-muted-800 dark:text-white"
+            class="text-muted-800 mb-2 dark:text-white"
           >
-            Iam a card
+            Muted card
           </BaseHeading>
 
-          <BaseParagraph
-            size="sm"
-            lead="tight"
-            class="text-muted-400"
-          >
+          <BaseParagraph size="sm" lead="tight" class="text-muted-400">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </BaseParagraph>
         </BaseCard>
 
-        <BaseCard
-          rounded="md"
-          color="info"
-          class="p-6"
-        >
+        <BaseCard variant="none" rounded="md" class="p-6 bg-primary-600/10 border-2 border-primary-600">
           <BaseHeading
             as="h4"
             size="sm"
             weight="semibold"
             lead="tight"
-            class="mb-2 text-muted-800 dark:text-white"
+            class="text-primary-600 mb-2 dark:text-primary-200"
           >
-            Iam a card
+            Custom card
           </BaseHeading>
 
-          <BaseParagraph
-            size="sm"
-            lead="tight"
-            class="text-muted-400"
-          >
+          <BaseParagraph size="sm" lead="tight" class="text-primary-600">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </BaseParagraph>
         </BaseCard>
 
-        <BaseCard
-          rounded="md"
-          color="success"
-          class="p-6"
-        >
+        <BaseCard variant="none" rounded="md" class="p-6 bg-muted-950 dark:bg-white">
           <BaseHeading
             as="h4"
             size="sm"
             weight="semibold"
             lead="tight"
-            class="mb-2 text-muted-800 dark:text-white"
+            class="text-white mb-2 dark:text-muted-900"
           >
-            Iam a card
+            Custom card
           </BaseHeading>
 
-          <BaseParagraph
-            size="sm"
-            lead="tight"
-            class="text-muted-400"
-          >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </BaseParagraph>
-        </BaseCard>
-
-        <BaseCard
-          rounded="md"
-          color="warning"
-          class="p-6"
-        >
-          <BaseHeading
-            as="h4"
-            size="sm"
-            weight="semibold"
-            lead="tight"
-            class="mb-2 text-muted-800 dark:text-white"
-          >
-            Iam a card
-          </BaseHeading>
-
-          <BaseParagraph
-            size="sm"
-            lead="tight"
-            class="text-muted-400"
-          >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </BaseParagraph>
-        </BaseCard>
-
-        <BaseCard
-          rounded="md"
-          color="danger"
-          class="p-6"
-        >
-          <BaseHeading
-            as="h4"
-            size="sm"
-            weight="semibold"
-            lead="tight"
-            class="mb-2 text-muted-800 dark:text-white"
-          >
-            Iam a card
-          </BaseHeading>
-
-          <BaseParagraph
-            size="sm"
-            lead="tight"
-            class="text-muted-400"
-          >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </BaseParagraph>
-        </BaseCard>
-
-        <BaseCard
-          rounded="md"
-          color="dark"
-          class="p-6"
-        >
-          <BaseHeading
-            as="h4"
-            size="sm"
-            weight="semibold"
-            lead="tight"
-            class="mb-2 text-muted-100 dark:text-muted-100"
-          >
-            Iam a card
-          </BaseHeading>
-
-          <BaseParagraph
-            size="sm"
-            lead="tight"
-            class="text-muted-400"
-          >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </BaseParagraph>
-        </BaseCard>
-
-        <BaseCard
-          rounded="md"
-          color="black"
-          class="p-6"
-        >
-          <BaseHeading
-            as="h4"
-            size="sm"
-            weight="semibold"
-            lead="tight"
-            class="mb-2 text-white dark:text-black"
-          >
-            Iam a card
-          </BaseHeading>
-
-          <BaseParagraph
-            size="sm"
-            lead="tight"
-            class="text-muted-400"
-          >
+          <BaseParagraph size="sm" lead="tight" class="text-white dark:text-muted-900">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </BaseParagraph>
         </BaseCard>
