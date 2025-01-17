d5149539b5fa677bae93460f6290e587d1937eed
Sacha STAFYNIAK <sacha@digisquad.io>
2025-01-18 00:22:21 +0100

    fix: meta component generation

--- packages/nuxt-component-meta/nuxt.config.ts
+++ packages/nuxt-component-meta/nuxt.config.ts
@@ -1,6 +1,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-08',
   modules: ['@shuriken-ui/nuxt', 'nuxt-component-meta'],
+  typescript: {
+    includeWorkspace: true,
+  },
   componentMeta: {
     globalsOnly: false,
     debug: 2,
